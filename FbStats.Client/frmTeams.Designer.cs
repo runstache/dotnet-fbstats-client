79dfea019f3d255fb703540748e421689aa1e36d
Larry Shields <larry.shields@gmail.com>
2020-10-10 20:54:53 -0400

    Added Teams Listing Screen

---
+++ FbStats.Client/frmTeams.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FbStats.Client
+{
+    partial class frmTeams
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstTeams = new System.Windows.Forms.ListBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstTeams
+            // 
+            this.lstTeams.FormattingEnabled = true;
+            this.lstTeams.ItemHeight = 31;
+            this.lstTeams.Location = new System.Drawing.Point(45, 31);
+            this.lstTeams.Name = "lstTeams";
+            this.lstTeams.Size = new System.Drawing.Size(701, 314);
+            this.lstTeams.TabIndex = 0;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(122, 374);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(207, 64);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(459, 374);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(207, 64);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmTeams
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lstTeams);
+            this.Name = "frmTeams";
+            this.Text = "Teams";
+            this.Load += new System.EventHandler(this.frmTeams_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstTeams;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ FbStats.Client/frmTeams.cs
@@ -0,0 +1,57 @@
+﻿using FbStats.Client.Factories;
+using FbStats.Client.Helpers;
+using FbStats.Data.DataObjects;
+using FbStats.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FbStats.Client
+{
+    public partial class frmTeams : Form
+    {
+        private IRepository repo;
+        public frmTeams()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTeams_Load(object sender, EventArgs e)
+        {
+            repo = RepositoryFactory.NewRepository();
+
+            try
+            {
+                List<Team> teams = repo.GetTeams().ToList();
+                lstTeams.DataSource = teams;
+                lstTeams.ValueMember = "Id";
+                lstTeams.DisplayMember = "TeamName";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            int teamId = TypeHelper.DefaultInteger(lstTeams.SelectedValue.ToString());
+            if (teamId > 0)
+            {
+                frmTeam teamForm = new frmTeam(teamId);
+                teamForm.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Select a Team.", "Select Teams", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
