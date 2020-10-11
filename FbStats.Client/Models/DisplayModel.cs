decc7166dc268a138a9e9e1059d02d91aa91b7e9
Larry Shields <larry.shields@gmail.com>
2020-10-10 20:55:37 -0400

    Added Display Model Class for Grid Views

---
+++ FbStats.Client/Models/DisplayModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FbStats.Client.Models
+{
+    public class DisplayModel
+    {
+        public long StatId { get; set; }
+        public string TeamName { get; set; }
+        public string OpponentName { get; set; }
+        public int WeekNumber { get; set; }
+
+
+    }
+}
