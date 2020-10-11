186b4d34d6abd6272626029e4f56ede30151ce76
Larry Shields <larry.shields@gmail.com>
2020-10-10 20:53:28 -0400

    Added Main Menu Screen

---
+++ FbStats.Client/frmMain.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FbStats.Client
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnTeams = new System.Windows.Forms.Button();
+            this.btnStats = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(254, 366);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(377, 96);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnTeams
+            // 
+            this.btnTeams.Location = new System.Drawing.Point(254, 70);
+            this.btnTeams.Name = "btnTeams";
+            this.btnTeams.Size = new System.Drawing.Size(377, 94);
+            this.btnTeams.TabIndex = 2;
+            this.btnTeams.Text = "Teams";
+            this.btnTeams.UseVisualStyleBackColor = true;
+            this.btnTeams.Click += new System.EventHandler(this.btnTeams_Click);
+            // 
+            // btnStats
+            // 
+            this.btnStats.Location = new System.Drawing.Point(254, 215);
+            this.btnStats.Name = "btnStats";
+            this.btnStats.Size = new System.Drawing.Size(377, 96);
+            this.btnStats.TabIndex = 3;
+            this.btnStats.Text = "Stats";
+            this.btnStats.UseVisualStyleBackColor = true;
+            this.btnStats.Click += new System.EventHandler(this.btnStats_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 571);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnStats);
+            this.Controls.Add(this.btnTeams);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnTeams;
+        private System.Windows.Forms.Button btnStats;
+    }
+}
+
---
+++ FbStats.Client/frmMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FbStats.Client
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTeams_Click(object sender, EventArgs e)
+        {
+            frmTeams teams = new frmTeams();
+            teams.Show();
+            
+        }
+
+        private void btnStats_Click(object sender, EventArgs e)
+        {
+            frmStats stats = new frmStats();
+            stats.Show();
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
