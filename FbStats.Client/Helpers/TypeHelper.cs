b72728c40b2498dc82d38c938b52e37af613d10d
Larry Shields <larry.shields@gmail.com>
2020-10-10 20:55:25 -0400

    Added Type Conversion Helper

---
+++ FbStats.Client/Helpers/TypeHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FbStats.Client.Helpers
+{
+    public static class TypeHelper
+    {
+        public static int DefaultInteger(string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                int.TryParse(value, out int converted);
+                return converted;
+            }
+            return 0;
+
+        }
+
+        public static double DefaultDouble(string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                double.TryParse(value, out double converted);
+                return converted;
+            }
+            return 0;
+        }
+
+        public static long DefaultLong(string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                long.TryParse(value, out long converted);
+                return converted;
+            }
+            return 0;
+        }
+    }
+}
