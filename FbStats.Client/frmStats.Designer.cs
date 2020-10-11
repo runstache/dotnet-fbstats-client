7a41359451326495c67a8d92a65c60eb04f1135c
Larry Shields <larry.shields@gmail.com>
2020-10-10 20:53:52 -0400

    Added Stats Screen for viewing Team Stats

---
+++ FbStats.Client/frmStats.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace FbStats.Client
+{
+    partial class frmStats
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdResults = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbTeam = new System.Windows.Forms.ComboBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdResults
+            // 
+            this.grdResults.AllowUserToAddRows = false;
+            this.grdResults.AllowUserToDeleteRows = false;
+            this.grdResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdResults.Location = new System.Drawing.Point(78, 206);
+            this.grdResults.MultiSelect = false;
+            this.grdResults.Name = "grdResults";
+            this.grdResults.ReadOnly = true;
+            this.grdResults.RowHeadersWidth = 102;
+            this.grdResults.RowTemplate.Height = 40;
+            this.grdResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdResults.Size = new System.Drawing.Size(1494, 364);
+            this.grdResults.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Team";
+            // 
+            // cmbTeam
+            // 
+            this.cmbTeam.FormattingEnabled = true;
+            this.cmbTeam.Location = new System.Drawing.Point(99, 117);
+            this.cmbTeam.Name = "cmbTeam";
+            this.cmbTeam.Size = new System.Drawing.Size(673, 39);
+            this.cmbTeam.TabIndex = 2;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(819, 106);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(167, 58);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(716, 616);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(185, 72);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(1101, 616);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(202, 72);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(173, 616);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(187, 72);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // frmStats
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1657, 750);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.cmbTeam);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grdResults);
+            this.Name = "frmStats";
+            this.Text = "Stats";
+            this.Load += new System.EventHandler(this.frmStats_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbTeam;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
---
+++ FbStats.Client/frmStats.cs
@@ -0,0 +1,116 @@
+﻿using FbStats.Client.Factories;
+using FbStats.Client.Helpers;
+using FbStats.Client.Models;
+using FbStats.Data.DataObjects;
+using FbStats.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FbStats.Client
+{
+    public partial class frmStats : Form
+    {
+        private IRepository repo;
+
+        public frmStats()
+        {
+            repo = RepositoryFactory.NewRepository();
+            InitializeComponent();
+        }
+
+        private void frmStats_Load(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                List<Team> teams = repo.GetTeams().OrderBy(c => c.TeamName).ToList();
+                cmbTeam.DataSource = teams;
+                cmbTeam.ValueMember = "Id";
+                cmbTeam.DisplayMember = "TeamName";
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int teamId = TypeHelper.DefaultInteger(cmbTeam.SelectedValue.ToString());
+                List<GameStat> stats = repo.GetStats(teamId).OrderBy(c => c.WeekNumber).ToList();
+                List<DisplayModel> models = new List<DisplayModel>();
+                foreach (GameStat stat in stats)
+                {
+                    Team team = repo.GetTeam(stat.TeamId);
+                    Team opp = repo.GetTeam(stat.OpponentId);
+
+                    DisplayModel model = new DisplayModel()
+                    {
+                        TeamName = team.TeamName,
+                        OpponentName = opp.TeamName,
+                        StatId = stat.Id,
+                        WeekNumber = stat.WeekNumber
+                        
+                    };
+                    models.Add(model);
+
+                }
+
+                grdResults.DataSource = models;
+                grdResults.Columns["StatId"].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DisplayModel model = (DisplayModel)grdResults.SelectedRows[0].DataBoundItem;
+                frmGameLog gameLog = new frmGameLog(model.StatId);
+                gameLog.Show();                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int teamId = TypeHelper.DefaultInteger(cmbTeam.SelectedValue.ToString());
+            if (teamId > 0)
+            {
+                frmAddStat add = new frmAddStat(teamId);
+                add.Show();
+            }
+            else
+            {
+                MessageBox.Show("Select a Team.", "Select Team", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+        }
+    }
+}
