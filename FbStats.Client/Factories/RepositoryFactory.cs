3d09603aaf8f0aa25405a55934265b79bfe67a8d
Larry Shields <larry.shields@gmail.com>
2020-10-10 20:55:08 -0400

    Added Repository Factory

---
+++ FbStats.Client/Factories/RepositoryFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using FbStats.Data.Repositories;
+
+namespace FbStats.Client.Factories
+{
+    public static class RepositoryFactory
+    {
+
+        public static IRepository NewRepository()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["statsdb"].ConnectionString;
+            return new SqlRepository(connectionString);
+        }
+
+    }
+}
