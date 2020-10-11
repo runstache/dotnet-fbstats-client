c0f6d05b6f36bb1f4ca4f659a1da7f2928afa13e
Larry Shields <larry.shields@gmail.com>
2020-10-10 20:54:08 -0400

    Added New Stat Window

---
+++ FbStats.Client/frmAddStat.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace FbStats.Client
+{
+    partial class frmAddStat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbOpponent = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbWeek = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbOpponent
+            // 
+            this.cmbOpponent.FormattingEnabled = true;
+            this.cmbOpponent.Location = new System.Drawing.Point(45, 200);
+            this.cmbOpponent.Name = "cmbOpponent";
+            this.cmbOpponent.Size = new System.Drawing.Size(567, 39);
+            this.cmbOpponent.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Opponent";
+            // 
+            // cmbWeek
+            // 
+            this.cmbWeek.FormattingEnabled = true;
+            this.cmbWeek.Location = new System.Drawing.Point(45, 105);
+            this.cmbWeek.Name = "cmbWeek";
+            this.cmbWeek.Size = new System.Drawing.Size(121, 39);
+            this.cmbWeek.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Week";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(91, 289);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(156, 63);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(300, 289);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(191, 63);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmAddStat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 436);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbWeek);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbOpponent);
+            this.Name = "frmAddStat";
+            this.Text = "Add Statistic";
+            this.Load += new System.EventHandler(this.frmAddStat_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbOpponent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbWeek;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ FbStats.Client/frmAddStat.cs
@@ -0,0 +1,95 @@
+﻿using FbStats.Client.Factories;
+using FbStats.Client.Helpers;
+using FbStats.Data.DataObjects;
+using FbStats.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FbStats.Client
+{
+    public partial class frmAddStat : Form
+    {
+        private IRepository repo;
+        private int teamId;
+        public frmAddStat(int teamId)
+        {
+            this.teamId = teamId;
+            repo = RepositoryFactory.NewRepository();
+            InitializeComponent();
+        }
+
+        private void frmAddStat_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadWeeks();
+                LoadTeams();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadTeams()
+        {
+            
+            List<Team> teams = repo.GetTeams().OrderBy(c => c.TeamName).ToList();
+            teams.RemoveAll(c => c.Id == teamId);
+            cmbOpponent.DataSource = teams;
+            cmbOpponent.DisplayMember = "TeamName";
+            cmbOpponent.ValueMember = "Id";
+
+        }
+
+        private void LoadWeeks()
+        {
+            cmbWeek.Items.Clear();
+            for (int i = 1; i < 25; i++)
+            {
+                cmbWeek.Items.Add(i);
+            }
+            
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            int week = TypeHelper.DefaultInteger(cmbWeek.SelectedItem.ToString());
+            int oppId = TypeHelper.DefaultInteger(cmbOpponent.SelectedValue.ToString());
+
+            if (week > 0 && oppId > 0)
+            {
+                GameStat stat = repo.GetStats(teamId).Where(c => c.OpponentId == oppId && c.WeekNumber == week).FirstOrDefault();
+                if (stat == null)
+                {
+                    stat = new GameStat();
+                    stat.TeamId = teamId;
+                    stat.OpponentId = oppId;
+                    stat.WeekNumber = week;
+                    stat = repo.Insert(stat);
+                    
+                }
+                frmGameLog gameLog = new frmGameLog(stat.Id);
+                gameLog.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Select a week and opponent", "Missing values", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
