530156bf3932c17cd16d0ed76c8c5ef472570b4d
Larry Shields <larry.shields@gmail.com>
2020-10-10 20:54:41 -0400

    Added Team Screen

---
+++ FbStats.Client/frmTeam.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace FbStats.Client
+{
+    partial class frmTeam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(56, 91);
+            this.txtCode.MaxLength = 4;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(146, 38);
+            this.txtCode.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(56, 206);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(553, 38);
+            this.txtName.TabIndex = 3;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(101, 288);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(176, 66);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(339, 288);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(176, 66);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmTeam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 392);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmTeam";
+            this.Text = "Team";
+            this.Load += new System.EventHandler(this.frmTeam_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ FbStats.Client/frmTeam.cs
@@ -0,0 +1,85 @@
+﻿using FbStats.Client.Factories;
+using FbStats.Data.DataObjects;
+using FbStats.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FbStats.Client
+{
+    public partial class frmTeam : Form
+    {
+        private IRepository repo;
+        private int teamId;
+        public frmTeam(int teamId)
+        {
+            repo = RepositoryFactory.NewRepository();
+            this.teamId = teamId;
+            InitializeComponent();
+        }
+
+        private void LoadTeam(int teamId)
+        {
+            Team team = repo.GetTeam(teamId);
+            txtCode.Text = team.TeamCode;
+            txtName.Text = team.TeamName;
+        }
+
+        private void SaveTeam()
+        {
+            Team team = new Team()
+            {
+                TeamCode = txtCode.Text,
+                TeamName = txtName.Text,
+                Id = teamId
+            };
+            repo.Update(team);
+
+        }
+
+        private void frmTeam_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadTeam(teamId);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtName.Text) && !string.IsNullOrEmpty(txtCode.Text))
+            {
+                try
+                {
+                    SaveTeam();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                frmTeams teamsForm = new frmTeams();
+                teamsForm.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Name and Code are required.", "Required Fields", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
